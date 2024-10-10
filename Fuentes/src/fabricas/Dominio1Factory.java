9be1e90ab06d99401139e0ee8f7b50893ce3b6f4
saabrii24 <sabrinabrugniere@gmail.com>
2024-10-10 14:56:28 -0300

    Actualiza fabricas

--- Fuentes/src/fabricas/Dominio1Factory.java
+++ Fuentes/src/fabricas/Dominio1Factory.java
@@ -1,5 +1,27 @@
 package fabricas;
 
-public class Dominio1Factory {
+/**
+ * La clase {@code Dominio1Factory} es una fábrica concreta que extiende la clase
+ * {@code SpritesFactory} y se utiliza para crear sprites específicos del dominio 1.
+ * Esta fábrica carga imágenes que representan a Mario.
+ */
+public class Dominio1Factory extends SpritesFactory {
 
+    /**
+     * Crea una instancia de {@code Dominio1Factory} que inicializa la ruta base de las
+     * imágenes del dominio 1 utilizadas para los sprites.
+     */
+    public Dominio1Factory() {
+        super("assets/imagenes/sprites/dominio1/mario_");
+    }
+
+    /**
+     * Devuelve una representación en cadena del nombre de los personajes del dominio 1.
+     *
+     * @return El nombre del personaje del dominio 1 como {@code String}, en este caso, "Mario".
+     */
+    @Override
+    public String toString() {
+        return "Mario";
+    }
 }
--- Fuentes/src/fabricas/Dominio2Factory.java
+++ Fuentes/src/fabricas/Dominio2Factory.java
@@ -1,5 +1,27 @@
 package fabricas;
 
-public class Dominio2Factory {
+/**
+ * La clase {@code Dominio2Factory} es una fábrica concreta que extiende la clase
+ * {@code SpritesFactory} y se utiliza para crear sprites específicos del dominio 2.
+ * Esta fábrica carga imágenes que representan a Luigi.
+ */
+public class Dominio2Factory extends SpritesFactory {
 
+    /**
+     * Crea una instancia de {@code Dominio2Factory} que inicializa la ruta base de las
+     * imágenes del dominio 2 utilizadas para los sprites.
+     */
+    public Dominio2Factory() {
+        super("assets/imagenes/sprites/dominio2/luigi_");
+    }
+
+    /**
+     * Devuelve una representación en cadena del nombre de los personajes del dominio 2.
+     *
+     * @return El nombre del personaje del dominio 2 como {@code String}, en este caso, "Luigi".
+     */
+    @Override
+    public String toString() {
+        return "Luigi";
+    }
 }

fbb3daccbff2b42c0fea2a1c50d29dd9f625e352
saabrii24 <sabrinabrugniere@gmail.com>
2024-10-19 16:23:09 -0300

    Actualiza paneles de victoria/derrota

--- Fuentes/src/gui/PanelPantallaVictoria.java
+++ Fuentes/src/gui/PanelPantallaVictoria.java
@@ -1,5 +1,30 @@
 package gui;
 
-public class PanelPantallaVictoria {
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelPantallaVictoria extends JPanel {
+
+    private static final long serialVersionUID = 1L;
+    private ControladorDeVistas controlador_vistas;
+    private JLabel imagen_fondo;
+
+    public PanelPantallaVictoria(ControladorDeVistas controlador_vistas) {
+        this.controlador_vistas = controlador_vistas;
+        setSize(ConstantesVistas.PANEL_ANCHO, ConstantesVistas.PANEL_ALTO);
+        setLayout(null);
+        agregar_imagen_fondo();
+    }
+
+    protected void agregar_imagen_fondo() {
+        imagen_fondo = new JLabel();
+        ImageIcon icono_imagen = new ImageIcon(this.getClass().getResource("/assets/imagenes/pantalla-victoria.png"));
+        Image imagen_escalada = icono_imagen.getImage().getScaledInstance(ConstantesVistas.PANEL_ANCHO, ConstantesVistas.PANEL_ALTO, Image.SCALE_SMOOTH);
+        Icon icono_imagen_escalado = new ImageIcon(imagen_escalada);
+        imagen_fondo.setIcon(icono_imagen_escalado);
+        setPreferredSize(new Dimension(ConstantesVistas.PANEL_ANCHO, ConstantesVistas.PANEL_ALTO));
+        imagen_fondo.setBounds(0, 0, ConstantesVistas.PANEL_ANCHO, ConstantesVistas.PANEL_ALTO);
+        add(imagen_fondo);
+    }
 
 }
